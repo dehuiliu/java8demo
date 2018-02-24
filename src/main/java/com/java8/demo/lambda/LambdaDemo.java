a299855ad088f5eaa302be6912790b1adfc56395
liudehui <huideliu@163.com>
2018-02-24 23:36:41 +0800

    java8 demo

---
+++ src/main/java/com/java8/demo/lambda/LambdaDemo.java
@@ -0,0 +1,13 @@
+package com.java8.demo.lambda;
+
+public class LambdaDemo {
+    /**
+     *  ->  将参数和lambda表达式的主体分开
+     */
+    public static void main(String[] args) {
+        Runnable test = () -> System.out.println("hello word");
+    }
+    public void test(){
+
+    }
+}
---
+++ src/test/java/com/java8/demo/lambda/LambdaDemoTest.java
@@ -0,0 +1,8 @@
+package com.java8.demo.lambda;
+
+public class LambdaDemoTest {
+    @org.junit.Test
+    public void test1() throws Exception {
+    }
+
+}
